a64124e225c5eb8b1df0448a319ec7c4398f31ae
Jong <tmdwhd619@naver.com>
2023-05-11 13:20:42 +0900

    [Fear] Fix the code

--- bluetooth_test.py
+++ bluetooth_test.py
@@ -1,56 +1,55 @@
 from bluetooth import *
- 
-#######################################################
-# Scan
-#######################################################
- 
-target_name = "iot_test"   # target device name
-target_address = None
-port = 1         # RFCOMM port
- 
+
+#########################
+## Scan
+##########################
+
+target_name = "Galaxy S8+" #target device name
+target_address = "94:8B:C1:01:E2:71"
+port = 1 #RFCOMM port
+
 nearby_devices = discover_devices()
- 
-# scanning for target device
+
 for bdaddr in nearby_devices:
-    print(lookup_name( bdaddr ))
-    if target_name == lookup_name( bdaddr ):
+    print(lookup_name(bdaddr))
+    if target_name == lookup_name(bdaddr):
         target_address = bdaddr
+        print("Update target address : ", target_address)
         break
- 
+
 if target_address is not None:
-    print('device found. target address %s' % target_address)
+    print("Device found.")
 else:
-    print('could not find target bluetooth device nearby')
- 
-#######################################################
-# Connect
-#######################################################
- 
-# establishing a bluetooth connection
+    print("Could not find target bluetooth device nearby")
+
+####################
+## Connect
+####################
+
 try:
-    sock=BluetoothSocket( RFCOMM )
-    sock.connect((target_address, port))
- 
-    while True:         
+    print("Try to connect Bluetooth socket")
+    sock = BluetoothSocket(RFCOMM)
+    print("RFCOMM : " ,RFCOMM)
+    sock.connect((target_address,port))
+    print("Success to connect target_address : ",target_address,"port : ", port)
+
+    sock.bind(("",port))
+    sock.listen(1)
+
+    client_socket, address = sock.accept()
+
+    while True:
         try:
-            recv_data = sock.recv(1024) #sock.recv(bufsize) ->maximum of buffer size
-
-            #receive Data: bytes type so we need to Encode/Decode when using with string
-            '''example
-            example of Encoding : my_bytes = b"Hello world"
-            example of Decoding : my_str = my_bytes.decode('utf-8')
-                                  my_str = str(bytes, 'utf-8')
-            '''
-            
-
-
-            print('bytes receive : ' ,recv_data)
-            print('encode receive : ' ,recv_data.decode('utf-8'))
-            #sock.send(recv_data) #rasberry -> smartphone
+            recv_data = client_socket.recv(1024)
+            msg = recv_data.decode()[:-2]
+            print("Received : %s" % msg)
+        
         except KeyboardInterrupt:
             print("disconnected")
             sock.close()
             print("all done")
-except btcommon.BluetoothError as err:
-    print('An error occurred : %s ' % err)
+
+except btcommon.BluetoothError as err :
+    print("An error occured : %s" %err)
+    sock.close()
     pass
